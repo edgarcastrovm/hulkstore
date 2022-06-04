07a276ae9d792db0dc806f9f3f712ca2adcbe0ac
Edgar Castro <edgar.castro.vm@gmail.com>
2022-06-04 12:16:21 -0500

    Initial Commit

---
+++ src/test/java/com/hulkstore/api/HulkStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulkstore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
